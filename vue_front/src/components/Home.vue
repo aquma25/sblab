ea802f4cdaebde7932f3660b5959462ca7523e3f
aquma25 <h.aquma25@gmail.com>
2021-01-17 16:59:03 +0900

    諸々の設定

---
+++ vue_front/src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <button>{{ msg }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+</style>
